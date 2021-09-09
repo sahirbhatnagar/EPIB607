98cdc8edfbf008ca908211ec4e76f62cdae24e7e
sahir <sahir.bhatnagar@gmail.com>
2021-09-09 00:44:46 -0400

    added two continous variables to data chapter

--- inst/figures/ch_intro_to_data_oi_biostat/figures/posNegCorPlots/posNegCorPlots.R
+++ inst/figures/ch_intro_to_data_oi_biostat/figures/posNegCorPlots/posNegCorPlots.R
@@ -1,7 +1,21 @@
 library(openintro)
 data(COL)
 data(possum)
-source("CorrelationPlot.R")
+CorrelationPlot <- function(x, y, ...) {
+  plot(x, y,
+       axes = FALSE,
+       pch = 20,
+       col = COL[1, 2],
+       cex = 1.351,
+       xlab = '',
+       ...)
+  box()
+  mtext(paste('R =', format(c(round(cor(x,y), 2), 0.01))[1]),
+        side = 1,
+        line = 1,
+        cex = 1.1)
+}
+
 set.seed(1)
 
 n <- 50
