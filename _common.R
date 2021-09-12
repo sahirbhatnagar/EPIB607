1d055733ccabf976511746ff2ef8606ff3642e93
sahir <sahir.bhatnagar@gmail.com>
2021-09-12 14:24:15 -0400

    adde github packages to _common.R

--- _common.R
+++ _common.R
@@ -10,4 +10,13 @@ knitr::opts_chunk$set(
   collapse = TRUE
   )
 
-
+if(!requireNamespace("oibiostat"))
+  devtools::install_github("OI-Biostat/oi_biostat_data")
+if(!requireNamespace("openintro"))
+  devtools::install_github("OpenIntroStat/openintro")
+if(!requireNamespace("cowplot"))
+  devtools::install_github("wilkelab/cowplot")
+if(!requireNamespace("colorblindr"))
+  devtools::install_github("clauswilke/colorblindr")
+if(!requireNamespace("dviz.supp"))
+  devtools::install_github("clauswilke/dviz.supp")
