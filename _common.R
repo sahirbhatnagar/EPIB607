267d213f286c74c469730f5b548958dce1b77146
sahir <sahir.bhatnagar@gmail.com>
2021-09-13 10:36:44 -0400

    install fonts with homebrew

--- _common.R
+++ _common.R
@@ -20,3 +20,12 @@ if(!requireNamespace("colorblindr"))
   remotes::install_github("clauswilke/colorblindr")
 if(!requireNamespace("dviz.supp"))
   remotes::install_github("clauswilke/dviz.supp")
+
+
+theme_dviz_grid_2 <- function(font = "Roboto") {
+  theme_dviz_grid(font_family = font)
+}
+
+theme_dviz_open_2 <- function(font = "Roboto") {
+  theme_dviz_open(font_family = font)
+}
