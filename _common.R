87f96f90db529f60a2d4fb3df05f079c6c53aa11
sahir <sahir.bhatnagar@gmail.com>
2021-09-13 12:03:51 -0400

    removed font family from dviz theme

--- _common.R
+++ _common.R
@@ -22,10 +22,23 @@ if(!requireNamespace("dviz.supp"))
   remotes::install_github("clauswilke/dviz.supp")
 
 
-theme_dviz_grid_2 <- function(font = "Roboto") {
-  theme_dviz_grid(font_family = font)
+theme_dviz_grid_2 <- function (font_size = 14, line_size = 0.5,
+                               rel_small = 12/14, rel_tiny = 11/14, rel_large = 16/14, colour = "grey90") {
+  half_line <- font_size/2
+  cowplot::theme_minimal_grid(font_size = font_size,
+                              line_size = line_size, rel_small = rel_small, rel_tiny = rel_tiny,
+                              rel_large = rel_large, colour = colour) %+replace% theme(plot.margin = margin(half_line/2,
+                                                                                                            1.5, half_line/2, 1.5), complete = TRUE)
 }
 
-theme_dviz_open_2 <- function(font = "Roboto") {
-  theme_dviz_open(font_family = font)
+
+
+theme_dviz_open_2 <- function (font_size = 14, line_size = 0.5,
+                               rel_small = 12/14, rel_tiny = 11/14, rel_large = 16/14) {
+  half_line <- font_size/2
+  cowplot::theme_half_open(font_size = font_size,
+                           line_size = line_size, rel_small = rel_small, rel_tiny = rel_tiny,
+                           rel_large = rel_large) %+replace% theme(plot.margin = margin(half_line/2,
+                                                                                        1.5, half_line/2, 1.5), complete = TRUE)
 }
+
