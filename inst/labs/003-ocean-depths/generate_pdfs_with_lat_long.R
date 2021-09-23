a287335399f194289fece9fef22132fe6caf45fa
sahir <sahir.bhatnagar@gmail.com>
2021-09-23 11:01:50 -0400

    2cha

--- inst/labs/003-ocean-depths/generate_pdfs_with_lat_long.R
+++ inst/labs/003-ocean-depths/generate_pdfs_with_lat_long.R
@@ -5,8 +5,8 @@ pacman::p_load(gmailr)
 
 # did this once only
 # gmailr::clear_token()
-gm_auth_configure(key = "956861284344-mavn6a7n6i1qsnckdi65s4rbtrm58m1a.apps.googleusercontent.com",
-                  path = "~/Downloads/client_secret_956861284344-mavn6a7n6i1qsnckdi65s4rbtrm58m1a.apps.googleusercontent.com.json")
+# gm_auth_configure(key = "956861284344-mavn6a7n6i1qsnckdi65s4rbtrm58m1a.apps.googleusercontent.com",
+#                   path = "~/Downloads/client_secret_956861284344-mavn6a7n6i1qsnckdi65s4rbtrm58m1a.apps.googleusercontent.com.json")
 pacman::p_load(dplyr)
 # pacman::p_load(purrr)
 
@@ -63,7 +63,11 @@ renderMyDocument <- function(email, id, name, ind5, ind20) {
 
 names(emails)
 emails[87,]
-BODY <- paste("Hello! See attached pdf for your randomly sampled latitudes and longitudes for the in-class exercise we will do on sampling distributions for means and proportions. Note that these latitudes and longitudes have been randomly sampled for each student in the class.")
+BODY <- paste("Hello!\n
+              See attached pdf for your randomly sampled latitudes and longitudes for the in-class exercise we will do on sampling distributions for means and proportions.\n
+              Note that these latitudes and longitudes have been randomly sampled for each student in the class.\n
+              This means that each student has a unique pdf with unique latitudes and longitudes.\n
+              Sahir")
 
 # first run the renderMyDocument to get all the pdfs, then comment that code and
 # run the gmail code
@@ -77,15 +81,17 @@ for(i in 1:nrow(emails)) {
   #                  ind20 = emails[i, "I_20_start"]:emails[i, "I_20_end"])
   # system(sprintf("rm /home/sahir/git_repositories/epib607/inst/labs/003-ocean-depths/students/%s_water_exercise_epib607.tex", emails[i,"ID"]))
 
+  BODY <- sprintf("Hi %s\nSee attached pdf for your randomly sampled latitudes and longitudes for the in-class exercise we will do on sampling distributions for means and proportions.\nNote that these latitudes and longitudes have been randomly sampled for each student in the class.\nThis means that each student has a unique pdf with unique latitudes and longitudes.\nSahir", emails[i,"Student.Name"])
+
   test_email <-
     gm_mime() %>%
-    gm_to("sahir.bhatnagar@mcgill.ca") %>%
-    # gm_to(emails[i, "Email"]) %>%
+    # gm_to("sahir.bhatnagar@mcgill.ca") %>%
+    gm_to(emails[i, "Email"]) %>%
     gm_from("sahir.bhatnagar@gmail.com") %>%
     gm_subject("[Fall 2021 - EPIB-607-001 - Inferential Statistics]: In-class exercise on sampling distributions") %>%
-    gm_html_body(BODY) %>%
-    # gm_attach_file(sprintf("/home/sahir/git_repositories/EPIB607/labs/003-ocean-depths/students/%s_water_exercise_epib607.pdf", emails[i,"ID"]))
-  gm_attach_file(sprintf("/home/sahir/git_repositories/epib607/inst/labs/003-ocean-depths/students/%s_water_exercise_epib607.pdf", emails[1,"ID"]))
+    gm_text_body(BODY) %>%
+    gm_attach_file(sprintf("/home/sahir/git_repositories/epib607/inst/labs/003-ocean-depths/students/%s_water_exercise_epib607.pdf", emails[i,"ID"]))
+  # gm_attach_file(sprintf("/home/sahir/git_repositories/epib607/inst/labs/003-ocean-depths/students/%s_water_exercise_epib607.pdf", emails[1,"ID"]))
 
 
   gm_send_message(test_email)
